b7917e2c087c8b924d9e829c82eee76bf6a66dc1
pandey27 <pandeynilesh861@gmail.com>
2025-06-24 23:10:45 +0530

    Wipe all tracked files from GitHub

--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  mobile: { type: String, required: true }, // this is your actual login identifier
-  address: String
-});
-
-module.exports = mongoose.model('User', userSchema);
